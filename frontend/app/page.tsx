c7756016479ace8481df4c01a9695adf49831247
LuisNunes301 <vnunes30@gmail.com>
2026-02-10 03:02:12 -0300

    add frontend minimal

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import VideoPlayer from "./components/VideoPlayer";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Mininetflix</h1>
+      <VideoPlayer />
+    </div>
+  );
+}
