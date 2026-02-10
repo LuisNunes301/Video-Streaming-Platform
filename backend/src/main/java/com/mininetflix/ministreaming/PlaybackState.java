aa9409301dc13f2081847a8a48597772abe5eb19
LuisNunes301 <vnunes30@gmail.com>
2026-02-10 02:38:56 -0300

    add minimal backend functional

---
+++ backend/src/main/java/com/mininetflix/ministreaming/PlaybackState.java
@@ -0,0 +1,17 @@
+package com.mininetflix.ministreaming;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlaybackState {
+    private String userId;
+    private String contentId;
+    private double currentTime;
+    private double duration;
+    private boolean completed;
+
+}
